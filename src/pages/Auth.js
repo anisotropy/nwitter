39da651edf09f86340786be9b641fb5c55ae1fb3
anisotropy <j.hodoug@aroundus.com>
2022-04-24 15:09:59 +0900

    Login form: part one

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,3 +1,36 @@
-const Auth = () => <div>Auth</div>;
+import { useState } from "react";
+
+const Auth = () => {
+  const [state, setState] = useState({ email: "", password: "" });
+
+  const onChange = (event) => {
+    setState((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          name="email"
+          type="email"
+          value={state.email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          value={state.password}
+          onChange={onChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+      <button>Login with Google</button>
+      <button>Login with GitHub</button>
+    </div>
+  );
+};
 
 export default Auth;
